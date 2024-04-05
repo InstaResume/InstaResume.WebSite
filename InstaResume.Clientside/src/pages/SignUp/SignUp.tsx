36692c8b6f37717af4be75542fbf818ed351370d
MyLife4What <greatzasin12@gmail.com>
2024-04-05 14:36:59 +0700

    Initialize signup page

--- InstaResume.Clientside/src/pages/SignUp/SignUp.tsx
+++ InstaResume.Clientside/src/pages/SignUp/SignUp.tsx
@@ -1,8 +1,117 @@
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  Grid,
+  Link,
+  TextField,
+  Typography,
+  alpha
+} from '@mui/material'
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
 import React from 'react'
 
-const SignUp = () => {
+const SignUp: React.FC = () => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const data = new FormData(event.currentTarget)
+    console.log({
+      email: data.get('email'),
+      password: data.get('password')
+    })
+  }
+
   return (
-    <div>Sign Up</div>
+    <Box
+      sx={(theme) => ({
+        width: '100%',
+        backgroundImage:
+          theme.palette.mode === 'light'
+            ? 'linear-gradient(180deg, #CEE5FD, #FFF)'
+            : `linear-gradient(#02294F, ${alpha('#090E10', 0.0)})`,
+        backgroundSize: '100% 20%',
+        backgroundRepeat: 'no-repeat',
+        height: '100vh'
+      })}
+    >
+      <Container
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          pt: { xs: 14, sm: 20 },
+          pb: { xs: 8, sm: 12 }
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign Up
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{}}>
+          <TextField
+            margin="dense"
+            required
+            fullWidth
+            id="name"
+            label="Name"
+            name="name"
+            autoComplete="name"
+            autoFocus
+            InputProps={{ style: { margin: '6px 0px' } }}
+          />
+          <TextField
+            margin="dense"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            InputProps={{ style: { margin: '6px 0px' } }}
+          />
+          <TextField
+            margin="dense"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            InputProps={{ style: { margin: '6px 0px' } }}
+          />
+          <TextField
+            margin="dense"
+            required
+            fullWidth
+            name="confirm-password"
+            label="Confirm Password"
+            type="confirm-password"
+            id="confirm-password"
+            InputProps={{ style: { margin: '6px 0px' } }}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign Up
+          </Button>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link href="/login" variant="body2">
+                {'Already have an account? Login'}
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Container>
+    </Box>
   )
 }
 
