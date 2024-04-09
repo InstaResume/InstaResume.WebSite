93a2fe5ad3a455af870b592d401f9b2f97dc2953
Tawan Boonma <tboonma@hotmail.com>
2024-04-10 00:26:34 +0700

    Cleanup

--- InstaResume.WebApi/Model/CreateResumeRequest.cs
+++ InstaResume.WebApi/Model/CreateResumeRequest.cs
@@ -7,12 +7,3 @@ public class CreateResumeRequest
     [JsonProperty("replacements")]
     public object Replacements { get; set; }
 }
-
-public class Replacement
-{
-    [JsonProperty("findText")]
-    public string FindText { get; set; }
-    
-    [JsonProperty("replaceText")]
-    public string ReplaceText { get; set; }
-}
